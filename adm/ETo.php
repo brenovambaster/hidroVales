122a31f15ac613bc8e78e3f7f5a3d417d3f74291
Breno Vambaster <brenovambaster5@gmail.com>
2019-10-23 15:18:32 -0300

    update

--- adm/ETo.php
+++ adm/ETo.php
@@ -23,20 +23,20 @@
             <div class=" form-row col-md-10 mx-auto">
                 <div class="form-group col-md-4">
                     <label for="tempMin">Temperatura mínima (C°):</label>
-                    <input type="number" class="form-control" name="tempMin" id="tempMin">
+                    <input type="number" class="form-control" required name="tempMin" id="tempMin">
 
                 </div>
                 <div class="form-group col-md-4">
                     <label for="tempMax">Temperatura máxima (C°):</label>
-                    <input type="number" class="form-control" name="tempMax" id="tempMax">
+                    <input type="number" class="form-control" required name="tempMax" id="tempMax">
                 </div>
                 <div class=" form-group col-md-4">
                     <label for="lat">Latitude:</label>
-                    <input type="float" name="lat" id="lat" class="form-control">
+                    <input type="float" name="lat" id="lat" required class="form-control">
                 </div>
                 <div class=" form-group col-md-4">
                     <label for="data">data:</label>
-                    <input type="date" name="data" id="data" class="form-control">
+                    <input type="date" name="data" id="data" required class="form-control">
                 </div>
 
             </div>
@@ -50,6 +50,10 @@
 
         </form>
     </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </body>
 
 
